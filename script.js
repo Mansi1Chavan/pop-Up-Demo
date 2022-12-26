8fbb2293efd89e90cc213e3ddd044b7a17ca755c
Mansi1Chavan/assignment <mansichavan5050@gmail.com>
2022-12-27 00:02:50 +0530

    Hello

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn1 = document.getElementById("btn1")
+     let btn2 = document.getElementById("btn2")
+     let ul = document.querySelector("ul")
+     btn1.addEventListener("click", ()=>{
+        ul.style.display = "block"
+         
+     })
+     btn2.addEventListener("click", ()=>{
+        ul.style.display = "none"
+         
+     })
